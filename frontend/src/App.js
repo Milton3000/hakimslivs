e1a52730287e594f3de1d023afd7be14a35b777a
MK3000 <kristofferssonmilton@gmail.com>
2024-03-31 12:33:50 +0200

    Frontend base structure done (components)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Categories from "./pages/Categories";
 
 function App() {
-    return ( <div className="App">
-<Router>
-<Routes>
-    <Route path="/" element={Home} />
-</Routes>
-</Router>
-    </div>
-    );
-}
+  return (
+    <div className="App">
 
+      <Router>
+        <Routes>
+            {/* Kanske behöver en till Route?  */}
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Router>
+
+    </div>
+  );
+}
 
 export default App;
