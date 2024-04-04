c72969e3c409a3608c3edcad97aea9f37a5c231c
MK3000 <kristofferssonmilton@gmail.com>
2024-04-04 15:58:17 +0200

    Hakim ändrat

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/" style={{ fontSize: '34px', color: 'blue' }}>Hakims Livs</Link>
+      <Link className="navbar-brand" to="/" style={{ fontSize: '34px', color: 'blue' }}>Hakim Livs</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
