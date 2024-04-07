b9a177ee52a92c22869f5d0eca08ddb33b9812c2
MK3000 <kristofferssonmilton@gmail.com>
2024-04-07 16:15:33 +0200

    Navbar Home "button" fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,9 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  // Reloadar sidan när man klickar på Hakim Livs
+  const handleHomeButtonClick = () => {
+    window.location.reload();
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/" style={{ fontSize: '34px', color: 'blue' }}>Hakim Livs</Link>
+      {/* Kör en href attribute till "/" */}
+      <a className="navbar-brand" href="/" onClick={handleHomeButtonClick} style={{ fontSize: '34px', color: 'blue' }}>Hakim Livs</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
