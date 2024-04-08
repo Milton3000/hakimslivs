78f9ebfec092251783c39e251e51340606123c50
MK3000 <kristofferssonmilton@gmail.com>
2024-04-08 20:50:31 +0200

    MODAL FIX

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Categories from './CategoriesLeft';
 import { Modal, Button } from 'react-bootstrap';
-import './Home.css'; 
+import './Home.css';
 
 const Home = ({ searchQuery }) => {
   const [products, setProducts] = useState([]);
@@ -12,8 +12,7 @@ const Home = ({ searchQuery }) => {
     // Fetch products from backend API
     const fetchProducts = async () => {
       try {
-        // const response = await fetch('/api/products/all'); 
-        const response = await fetch('https://hakimslivs-backend.onrender.com/api/products/all'); 
+        const response = await fetch('https://hakimslivs-backend.onrender.com/api/products/all');
         const data = await response.json();
         setProducts(data);
       } catch (error) {
@@ -46,9 +45,14 @@ const Home = ({ searchQuery }) => {
     setSelectedProduct(null);
   };
 
+  const handleAddToCartClick = (event, productId) => {
+    event.stopPropagation(); // Prevent the modal from opening when "Lägg till i Varukorg" button is clicked
+    addToCart(productId);
+  };
+
   return (
     <div className="home">
-      <div className="category-section"> 
+      <div className="category-section">
         <Categories setProducts={setProducts} />
       </div>
       <div className="product-section">
@@ -57,19 +61,16 @@ const Home = ({ searchQuery }) => {
             <h3 className='gradient_text'>POPULÄRT JUST NU</h3>
           </div>
         </div>
-        <div className="row row-cols-2 row-cols-md-6 g-4">
+        <div className="row row-cols-1 row-cols-md-4 g-4">
           {filteredProducts.map((product, index) => (
-            <div key={index} className="col">
-              <div className="card h-100 product-card" onClick={() => handleProductClick(product)}>
-                <img src={product.imageUrl} className="card-img-top img-fluid mt-3 product-image" alt={product.title} />
-                <div className="card-body d-flex flex-column">
-                  <h5 className="card-title fs-5 product-title">{product.title}</h5>
-                  <h6 className="card-text fs-6 mb-3">{product.supplier}</h6>
-                  <div className="product-description-scroll">
-                    <p className="card-text fs-6 mb-3">{product.description}</p>
-                  </div>
-                  <p className="card-text fs-6 mb-5 product-price">Pris: {product.price} SEK</p>
-                  <button onClick={() => addToCart(product.id)} className="btn btn-primary mt-auto">Lägg till i varukorg</button>
+            <div key={index} className="col" onClick={() => handleProductClick(product)}>
+              <div className="card product-card">
+                <img src={product.imageUrl} className="card-img-top img-fluid mt-3" alt={product.title} style={{ objectFit: 'contain', maxHeight: '200px', maxWidth: '100%' }} />
+                <div className="card-body d-flex flex-column align-items-center" style={{ minHeight: '250px' }}>
+                  <h5 className="card-title fs-5 product-title text-center">{product.title}</h5>
+                  <h6 className="card-text fs-6 text-muted mb-3 text-center">{product.supplier}</h6>
+                  <p className="card-text fs-6 mb-3 product-price text-center">Pris: {product.price} SEK</p>
+                  <button onClick={(e) => handleAddToCartClick(e, product.id)} className="btn btn-primary">Lägg till i varukorg</button>
                 </div>
               </div>
             </div>
@@ -77,14 +78,14 @@ const Home = ({ searchQuery }) => {
         </div>
       </div>
       {selectedProduct && (
-        <Modal show={true} onHide={handleCloseModal} size="sm">
+        <Modal show={true} onHide={handleCloseModal} size="md">
           <Modal.Header closeButton>
-            <Modal.Title>{selectedProduct.title}</Modal.Title>
+            <Modal.Title className="text-center">{selectedProduct.title}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <img src={selectedProduct.imageUrl} alt={selectedProduct.title} className="img-fluid" />
-            <div className="scrollable-details">
-              <p>{selectedProduct.description}</p>
+            <img src={selectedProduct.imageUrl} alt={selectedProduct.title} className="img-fluid mx-auto d-block" style={{ maxHeight: '350px' }} />
+            <div className="scrollable-details text-center">
+              <p>{selectedProduct.description.length > 100 ? `${selectedProduct.description.substring(0, 100)}...` : selectedProduct.description}</p>
               <p>Pris: {selectedProduct.price} SEK</p>
               <Button variant="primary" onClick={() => addToCart(selectedProduct.id)}>Lägg till i varukorg</Button>
             </div>
