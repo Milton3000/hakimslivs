da44606ec1538116d6996c062fdbd47d1297b7fa
mazmann <sadi.lundberg@hotmail.com>
2024-04-18 00:03:03 +0200

    started on add product to order via regex search

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createProduct, deleteProduct, getProduct, getProducts, updateProduct, getProductByCategory } from '../controllers/product.controller.js';
+import { createProduct, getProductsByName, deleteProduct, getProduct, getProducts, updateProduct, getProductByCategory } from '../controllers/product.controller.js';
 const productRouter = express.Router();
 
 productRouter.get('/all', getProducts);
+productRouter.get('/', getProductsByName);
 productRouter.get('/product/:id', getProduct);
 productRouter.get('/category/:category', getProductByCategory);
 productRouter.post('/new', createProduct);
