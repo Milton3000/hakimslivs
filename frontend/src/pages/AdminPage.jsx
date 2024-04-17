10d3db76b57aa70e5c608e1db579a14f59c79a1f
mazmann <sadi.lundberg@hotmail.com>
2024-04-17 09:29:08 +0200

    updated layout

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -7,6 +7,7 @@ import {
 } from '@tanstack/react-query';
 import '../styles/adminPage.css';
 import profilepic from '../styles/profile-circle.svg';
+import 'iconify-icon';
 
 const AdminPage = () => {
   const [activePanel, setActivePanel] = useState('products');
@@ -17,18 +18,21 @@ const AdminPage = () => {
 
   return (
     <div style={{ display: 'flex', height: '50rem' }}>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div style={{ display: 'flex', alignItems: 'center', paddingLeft: '15px' }}>
         <div className='admin-menu'>
-          <div style={{ width: '141px' }} className='user-box'>
+          <div className='user-box'>
             <img style={{ height: '75px', width: '75px' }} src={profilepic} alt="Profile" />
             <b>Användare</b>
           </div>
           <ul className='admin-ul'>
-            <li className='admin-list-item' onClick={() => handlePanelChange('products')}>Produkthantering</li>
-            <li className='admin-list-item' onClick={() => handlePanelChange('orders')}>Orderhantering</li>
-            <li className='admin-list-item'>(tom)</li>
-            <li className='admin-list-item'>(tom)</li>
-            <li className='admin-list-item'>(tom)</li>
+            <li className='admin-list-item' onClick={() => handlePanelChange('products')}>
+            <iconify-icon icon="dashicons:edit" width="24" height="24"  style={{color: '#3184dd'}} />
+              Produkthantering
+              </li>
+            <li className='admin-list-item' onClick={() => handlePanelChange('orders')}>
+            <iconify-icon icon="dashicons:list-view" width="24" height="24"  style={{color: '#3184dd'}} />
+              Orderhantering
+              </li>
           </ul>
         </div>
       </div>
