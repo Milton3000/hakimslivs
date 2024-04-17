ceced0d5df3c46bac9b176790b7dee4aed0bf3f4
mazmann <sadi.lundberg@hotmail.com>
2024-04-17 22:06:44 +0200

    added delete product, add product and delete order to BE

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createOrder, getOrders, updateOrder } from '../controllers/order.controller.js'
+import { createOrder, getOrders, updateOrder, addProductToOrder, deleteProductFromOrder, deleteOrder } from '../controllers/order.controller.js'
 const orderRouter = express.Router();
 
 orderRouter.post('/neworder', createOrder);
@@ -7,5 +7,11 @@ orderRouter.post('/neworder', createOrder);
 orderRouter.get('/allorders', getOrders);
 
 orderRouter.put('/update/:id', updateOrder);
+
+orderRouter.put('/addproduct/:id', addProductToOrder);
+
+orderRouter.put('/deleteproduct/:id/:productId', deleteProductFromOrder);
+
+orderRouter.delete('/delete/:id', deleteOrder);
 
 export { orderRouter };
