cd06f2a357595c776dcde7c9d43d18ca952ce11e
dadar870218 <dadar870218@gmail.com>
2024-04-18 12:02:50 +0200

    changes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { userLogin } from '../controllers/auth.controller.js';
+import { userLogin, registerUser } from '../controllers/auth.controller.js';
+
 const authRouter = express.Router();
 
-authRouter.post('/userLogin', userLogin);
+authRouter.post('/login', userLogin);
+authRouter.post('/register', registerUser);
 
 export { authRouter };
