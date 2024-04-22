c38a48536a435f9d1d3766102a71f2a573f1128e
MK3000 <kristofferssonmilton@gmail.com>
2024-04-22 19:32:59 +0200

    Cors allowedOrigins added + url to see if it works on deployed version

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,30 +11,31 @@ import { userRouter } from './routes/user.route.js'
 // Load environment variables from .env file
 dotenv.config();
 
-
 const app = express();
 
-// Enable CORS middleware
-app.use(cors()); 
+// Define allowed origins
+const allowedOrigins = ['https://hakimslivs.vercel.app'];
 
+// Enable CORS middleware with allowed origins
+app.use(cors({
+  origin: allowedOrigins
+}));
 
-// Apply middleware för att konvertera till json när man skickar data från frontend. 
+// Apply middleware to convert data to JSON when sent from the frontend
 app.use(express.json());
 
-
-app.use("/api/auth", authRouter); // Endpoint för authentication (routes > users.js)
+app.use("/api/auth", authRouter); // Endpoint for authentication (routes > users.js)
 app.use("/api/products", productRouter);
 app.use("/api/customers", customerRouter);
 app.use("/api/orders", orderRouter);
 app.use("/api/users", userRouter);
 
-// Använder environment variable för MongoDB Password.
+// Use environment variable for MongoDB Password
 const mongoDBPassword = process.env.MONGODB_PASSWORD;
 
 const mongoDBURI = `mongodb+srv://Milton:${mongoDBPassword}@hakimcluster.jkjs7d2.mongodb.net/hakimslivs`;
 
 mongoose.connect(mongoDBURI);
-
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
