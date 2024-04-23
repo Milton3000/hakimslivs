a8b6ee0086bbe2236390101069cd1e6002940f04
MK3000 <kristofferssonmilton@gmail.com>
2024-04-23 22:21:40 +0200

    Footer centrerad (kan skippa, test bara)

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,25 +4,25 @@ const Footer = () => {
   return (
     <footer className="bg-light text-dark py-4">
       <div className="container">
-        <div className="row">
+        <div className="row justify-content-center"> 
           <div className="col-md-4">
-            <h5 className="mb-4">Kontakt</h5>
-            <ul className="list-unstyled">
+            <h5 className="mb-4 text-center">Kontakt</h5> 
+            <ul className="list-unstyled text-center"> 
               <li><strong>Telefon:</strong> 0123-456789</li>
               <li><strong>Email:</strong> info@hakimlivs.se</li>
               <li><strong>Adress:</strong> Storgatan 123, Stockholm</li>
             </ul>
           </div>
           <div className="col-md-4">
-            <h5 className="mb-4">Öppettider</h5>
-            <ul className="list-unstyled">
+            <h5 className="mb-4 text-center">Öppettider</h5>
+            <ul className="list-unstyled text-center"> 
               <li><strong>Mån-Fre:</strong> 09:00-18:00</li>
               <li><strong>Lör-Sön:</strong> Stängt</li>
             </ul>
           </div>
           <div className="col-md-4">
-            <h5 className="mb-4">Betalsätt</h5>
-            <ul className="list-unstyled">
+            <h5 className="mb-4 text-center">Betalsätt</h5> 
+            <ul className="list-unstyled text-center"> 
               <li><strong>Bankgiro:</strong> 123-4567</li>
               <li><strong>Swish:</strong> 1234567890</li>
               <li><strong>Kortbetalning:</strong> Visa, Mastercard</li>
