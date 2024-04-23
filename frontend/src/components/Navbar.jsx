93e8890cccfdd1017c5f434f2717b79edf32be22
dadar870218 <dadar870218@gmail.com>
2024-04-23 21:23:24 +0200

    rensa sökning , snabb fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaShoppingCart } from 'react-icons/fa'; // Cart Icon
 import SearchBar from './SearchBar';
 
 const Navbar = ({ onSearchInputChange, toggleCart, totalCartItems }) => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearchInputChange = (value) => {
+    setSearchValue(value);
+    onSearchInputChange(value);
+  };
+
+  const handleClearSearch = () => {
+    setSearchValue('');
+    onSearchInputChange('');
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top"> {/* Add 'sticky-top' class */}
+    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
       <a className="navbar-brand" href="/" onClick={(e) => { e.preventDefault(); window.location.href = "/"; }} style={{ fontSize: '34px', color: 'blue' }}>Hakim Livs</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-        </ul>
-        <SearchBar onSearchInputChange={onSearchInputChange} />
-        {/* Cart Icon här */}
+        <ul className="navbar-nav mr-auto"></ul>
+        <SearchBar 
+          onSearchInputChange={handleSearchInputChange} 
+          searchValue={searchValue}
+          handleClearSearch={handleClearSearch}
+        />
         <ul className="navbar-nav">
           <li className="nav-item">
             <button className="nav-link cart-icon" onClick={toggleCart} style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
@@ -24,7 +38,6 @@ const Navbar = ({ onSearchInputChange, toggleCart, totalCartItems }) => {
             </button>
           </li>
         </ul>
-
       </div>
     </nav>
   );
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-const SearchBar = ({ onSearchInputChange }) => {
+const SearchBar = ({ onSearchInputChange, searchValue, handleClearSearch }) => {
   return (
-    <div className="mx-auto" style={{ maxWidth: "300px" }}>
+    <div className="mx-auto" style={{ width: "300px" }}>
       <div className="input-group">
         <input
           className="form-control"
           type="search"
-          placeholder="Search"
-          aria-label="Search"
+          placeholder="Sök"
+          aria-label="Sök"
+          value={searchValue}
           onChange={(e) => onSearchInputChange(e.target.value)}
         />
-        <div className="input-group-append">
-          <button
-            className="btn btn-outline-success"
-            type="button"
-          >
-            Sök
+      </div>
+      {searchValue && (
+        <div className="mt-2">
+          <button className="btn btn-outline-secondary w-100" type="button" onClick={handleClearSearch}>
+            Rensa sökning
           </button>
         </div>
-      </div>
+      )}
     </div>
   );
 }
