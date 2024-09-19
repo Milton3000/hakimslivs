bf673290e6a2079f7f5af8af3e97084ae45a8a03
Milton3000 <kristofferssonmilton@gmail.com>
2024-09-19 18:20:34 +0200

    New seed added

--- backend/seedData.js
+++ backend/seedData.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import Product from './src/models/product.model.js'; // Make sure the path to your model is correct
+import Product from './src/models/product.model.js'; // Ensure the path to your model is correct
 import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
@@ -8,13 +8,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // MongoDB connection setup
-const mongoDBURI = process.env.MONGODB_URI || `mongodb+srv://Milton:${mongoDBPassword}@bustercluster.nso9m.mongodb.net/hakimslivs`;
+const mongoDBPassword = process.env.MONGODB_PASSWORD;
+const mongoDBURI = `mongodb+srv://Milton:${mongoDBPassword}@bustercluster.nso9m.mongodb.net/hakimslivs`;
 
 mongoose.connect(mongoDBURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
 
 const db = mongoose.connection;
 
@@ -22,17 +22,27 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", async () => {
     console.log("Connected to MongoDB database!");
 
-    // Load product data from JSON file
-    const productsToAdd = JSON.parse(fs.readFileSync(path.resolve('./data/frukt.json'), 'utf-8'));
+    // List of JSON files to read, excluding frukt.json
+    const jsonFiles = [
+        './data/chark.json', 
+        './data/dryck.json', 
+        './data/mejeri.json', 
+        './data/snacks&godis.json', 
+        './data/torrvaror.json'
+    ];
 
-    // Loop through productsToAdd and add each product to the database
-    for (const productData of productsToAdd) {
-        try {
-            const product = new Product(productData);
-            const savedProduct = await product.save();
-            console.log("Product added to the database:", savedProduct);
-        } catch (err) {
-            console.error("Error saving product:", err);
+    // Loop through each JSON file and add its contents to the database
+    for (const file of jsonFiles) {
+        const productsToAdd = JSON.parse(fs.readFileSync(path.resolve(file), 'utf-8'));
+
+        for (const productData of productsToAdd) {
+            try {
+                const product = new Product(productData);
+                const savedProduct = await product.save();
+                console.log("Product added to the database:", savedProduct);
+            } catch (err) {
+                console.error("Error saving product:", err);
+            }
         }
     }
     
